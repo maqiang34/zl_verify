3cc5dcab20a275f0ee4d87f32833404cce111399
royadli735@163.com <royadli735@163.com>
2019-01-25 15:59:29 +0800

    1.25 验证report新结构

--- cold-verify-web/src/main/java/com/smartcold/manage/cold/controller/report/ReportController.java
+++ cold-verify-web/src/main/java/com/smartcold/manage/cold/controller/report/ReportController.java
@@ -1,4 +1,4 @@
-package com.smartcold.manage.cold.controller;
+package com.smartcold.manage.cold.controller.report;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
